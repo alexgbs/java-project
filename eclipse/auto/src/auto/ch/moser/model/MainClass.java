39a08d3b6830ac37122fa4f10409940c37687116
Ehrenhoefer <Ehrenhoefer.Alex@gbsinf2.lokal>
2016-11-18 14:25:13 +0100

    de damian het en chline

---
+++ eclipse/auto/src/auto/ch/moser/model/MainClass.java
@@ -0,0 +1,13 @@
+package auto.ch.moser.model;
+
+import auto.ch.moser.controller.AutoZusammenSteller;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		AutoZusammenSteller auto1 = new AutoZusammenSteller(System.in);
+		
+	}
+
+}
