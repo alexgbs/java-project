9c30c4c6e3b9532d79eafe393296fa74adcaeb8d
Ehrenhoefer <Ehrenhoefer.Alex@gbsinf2.lokal>
2016-11-25 17:07:18 +0100

    💩💩💩 de DAMIAN het en CHLLIIINE 💩💩💩💩💩💩💩💩💩💩💩💩

--- auto/src/auto/ch/moser/MainClass.java
+++ auto/src/auto/ch/moser/MainClass.java
@@ -5,7 +5,12 @@ import auto.ch.moser.controller.AutoZusammenSteller;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("#################################");
+		System.out.println("  /####################################################\\");
+		System.out.println(" /###########                              #############\\");
+		System.out.println("--###########        AUTOWERKSTATT         ##############--");
+		System.out.println(" \\###########                              #############/");
+		System.out.println("  \\####################################################/");
+		System.out.println();
 		AutoZusammenSteller auto1 = new AutoZusammenSteller(System.in);
 		System.out.println(auto1);
 	}
